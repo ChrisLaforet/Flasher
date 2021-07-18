fb3f9e5724db9ca75158408c1552546838470db8
Chris Laforet <laforet@chrislaforetsoftware.com>
2021-07-18 15:04:06 -0400

    Starting design on file selection

---
+++ app/src/main/java/com/chrislaforetsoftware/flasher/FileSelection.kt
@@ -0,0 +1,11 @@
+package com.chrislaforetsoftware.flasher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FileSelection : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_file_selection)
+	}
+}

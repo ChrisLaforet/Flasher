d2a3f561233bae4a86e6c79b03670a1b51061fd4
Chris Laforet <laforet@chrislaforetsoftware.com>
2021-08-02 15:25:29 -0400

    Initial work towards file picker activity

--- app/src/main/java/com/chrislaforetsoftware/flasher/pickers/FilePicker.kt
+++ app/src/main/java/com/chrislaforetsoftware/flasher/pickers/FilePicker.kt
@@ -1,7 +1,6 @@
 package com.chrislaforetsoftware.flasher.pickers
 
 import android.Manifest
-import android.app.Activity;
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
@@ -14,11 +13,16 @@ import com.chrislaforetsoftware.flasher.R
 import java.io.File
 import java.util.*
 
+private data class PathContainer(var basePath: File, var newPath: File) {}
+
 class FilePicker(): AppCompatActivity() {
 
     private lateinit var folderList: ListView
     private lateinit var filesList: ListView
-    private val stack: Deque<File> = ArrayDeque<File>()
+
+    private val stack: Deque<PathContainer> = ArrayDeque<PathContainer>()
+    private lateinit var rootPath: File
+    private var currentPath: String = File.separator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +41,52 @@ class FilePicker(): AppCompatActivity() {
         }
 
         folderList = this.findViewById(R.id.folderList)
+        folderList.setOnItemClickListener { parent, view, position, id ->
+            val element = parent.getItemAtPosition(position) 	// The item that was clicked
+            val folderName = element.toString()
+            var folder: File? = null
+            if (folderName.startsWith(".. ")) {
+                val previousPath = stack.pop()
+                folder = previousPath.basePath
+                currentPath = getCurrentPath(true)
+                if (currentPath.isEmpty()) {
+                    currentPath = File.separator
+                }
+            } else {
+                folder = File(createNewPathFromCurrentPath(folderName))
+                stack.push(PathContainer(File(getCurrentPath()), folder))
+                currentPath = createNewPathFromCurrentPath(folderName, true)
+            }
+            showCurrentPath(folder ?: rootPath)
+        }
         filesList = this.findViewById(R.id.filesList)
 
-//        val roots = File.listRoots()
-//        showCurrentPath(roots[0])
-//        showCurrentPath(File("/sdcard"))
-        showCurrentPath(Environment.getExternalStorageDirectory())
-//        showCurrentPath(File("/sdcard"))
+        rootPath = Environment.getExternalStorageDirectory()
+        showCurrentPath(rootPath)
+    }
+
+    private fun getCurrentPath(excludeRootPath: Boolean = false): String {
+        val builder: StringBuilder = StringBuilder()
+        if (!excludeRootPath) {
+            builder.append(rootPath.absolutePath)
+        }
+
+        stack.toList().forEach {
+            if (it.basePath != rootPath) {
+                builder.append(File.separator)
+                builder.append(it.basePath.name)
+            }
+        }
+
+        return builder.toString()
+    }
+
+    private fun createNewPathFromCurrentPath(filename: String, excludeRootPath: Boolean = false): String {
+        val builder: StringBuilder = StringBuilder()
+        builder.append(getCurrentPath(excludeRootPath))
+        builder.append(File.separator)
+        builder.append(filename)
+        return builder.toString()
     }
 
     private fun setupPermissions(): Boolean {
@@ -77,7 +120,7 @@ class FilePicker(): AppCompatActivity() {
 
             showFolders(folders)
             showFiles(files)
-
+            showCurrentPath()
         } catch (ex: Exception) {
             Toast.makeText(baseContext,
                     ex.message,
@@ -117,5 +160,10 @@ class FilePicker(): AppCompatActivity() {
                 listItems
         )
         filesList.adapter = adapter
+    }
+
+    private fun showCurrentPath() {
+        val ab = supportActionBar
+        ab!!.subtitle = currentPath
     }
 }
